a9cf62b0e0052275e3d0169651f5a069c1ae0c91
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2022-10-06 11:18:12 -0300

    feat: Validating Filme Model fiels

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,18 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
+
         public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duração deve ter entre 1 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
