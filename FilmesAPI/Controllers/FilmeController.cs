76c4595ea4ad2f129a8ba5b8e2b6c12ab99bb566
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2022-10-07 11:04:00 -0300

    feat: Connecting to DataBase

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -10,9 +10,31 @@ namespace FilmesAPI.Controllers
         private static List<Filme> filmes = new List<Filme>();
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id++;
             filmes.Add(filme);
+
+            return CreatedAtAction(nameof(RecuperarFilmePorId), new {Id = filme.Id}, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+
+            return NotFound();
         }
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,13 +4,18 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "O campo título é obrigatório")]
-        public string Titulo { get; set; }
+        public string? Titulo { get; set; }
 
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
-        public string Diretor { get; set; }
+        public string? Diretor { get; set; }
 
-        public string Genero { get; set; }
+        [StringLength(50, ErrorMessage = "O gênero deve ter no máximo 50 caracteres")]
+        public string? Genero { get; set; }
 
         [Range(1, 600, ErrorMessage = "A duração deve ter entre 1 e 600 minutos")]
         public int Duracao { get; set; }
