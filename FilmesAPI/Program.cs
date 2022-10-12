f6fd266fe135eac9767b93229288c34a757810f0
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2022-10-12 12:03:27 -0300

    refactor: Adding Scope

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,4 +1,5 @@
 using FilmesAPI.Data;
+using FilmesAPI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opts => opts.UseLazyLoadingProxies().UseMySql(builder.Configuration.GetConnectionString("FilmeConnection"), new MySqlServerVersion(new Version(8, 0))));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<FilmeService, FilmeService>();
+builder.Services.AddScoped<CinemaService, CinemaService>();
+builder.Services.AddScoped<EnderecoService, EnderecoService>();
+builder.Services.AddScoped<GerenteService, GerenteService>();
+builder.Services.AddScoped<SessaoService, SessaoService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
